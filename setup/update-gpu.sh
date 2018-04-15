58eb1713a25f227a07bb82f9011e632feae25270
Ubuntu <ubuntu@ip-10-0-0-5.eu-west-1.compute.internal>
2018-04-15 11:19:52 +0000

    done with more sh scripts

---
+++ setup/update-gpu.sh
@@ -0,0 +1,8 @@
+# This script is only tested with ubuntu 14.04 LTS
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install emacs24-nox
+sudo apt-get install kaggle-cli
+sudo apt-get install unzip
+sudo reboot
+
+# Then for kaggle-cli to work, need to setup password and competition

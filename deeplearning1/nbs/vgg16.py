e8efa1178e5752e8168921cb80939fee2977ff27
Ubuntu <ubuntu@ip-10-0-0-9.eu-west-1.compute.internal>
2018-04-17 05:12:01 +0000

    update vgg16.py file

--- deeplearning1/nbs/vgg16.py
+++ deeplearning1/nbs/vgg16.py
@@ -22,7 +22,7 @@ K.set_image_dim_ordering('th')
 
 vgg_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32).reshape((3,1,1))
 def vgg_preprocess(x):
-        """
+    """
         Subtracts the mean RGB value, and transposes RGB to BGR.
         The mean RGB was computed on the image set used to train the VGG model.
 
@@ -57,7 +57,7 @@ class Vgg16():
         self.classes = [class_dict[str(i)][1] for i in range(len(class_dict))]
 
     def predict(self, imgs, details=False):
-         """
+        """
             Predict the labels of a set of images using the VGG16 model.
 
             Args:
@@ -114,6 +114,7 @@ class Vgg16():
     def create(self):
         """
             Creates the VGG16 network achitecture and loads the pretrained weights.
+            You can modify input shape, and pop the last layer to change the number of classes.
 
             Args:   None
             Returns:   None

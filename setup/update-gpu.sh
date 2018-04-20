a7a9807382ca0ea86761d8980bf436b5958d0ead
nguyentu1602 <cuong.nguyentumanh@gmail.com>
2018-04-20 17:20:38 +0100

    add g3 for aws-alias.sh

--- setup/update-gpu.sh
+++ setup/update-gpu.sh
@@ -1,8 +1,11 @@
 # This script is only tested with ubuntu 14.04 LTS
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get -y update && sudo apt-get dist-upgrade
 sudo apt-get install emacs24-nox
-sudo apt-get install kaggle-cli
 sudo apt-get install unzip
+
+pip install --upgrade pip
+pip install kaggle-cli
+
 sudo reboot
 
 # Then for kaggle-cli to work, need to setup password and competition
